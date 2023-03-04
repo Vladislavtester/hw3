8768c665ccf7ccbb149e930dd93b9a0dbfefa7e0
Vladislav Nazarov <nazzzap@gmail.com>
2023-03-04 22:11:21 +0600

    added .Testformfill

--- src/test/java/TestFormFill.java
+++ src/test/java/TestFormFill.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.Find;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,12 +16,30 @@ static void beforeAll(){
     Configuration.baseUrl = "https://demoqa.com" ;   }
 
     @Test
-    void fillFormTest(){
+    void fillFormTest() throws InterruptedException {
         open("/automation-practice-form");
 
         $("#firstName").setValue("Vova");
         $("#lastName").setValue("Kashin");
         $("#userEmail").setValue("Kashin@mail.ru");
-        $("#userEmail").setValue("Kashin@mail.ru");
-        $("#submit").click();
+        $("#gender-radio-1").sendKeys(" ");
+        $("#userNumber").val("12345");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__day--015").click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("2007");
+        $(".subjects-auto-complete__value-container").click();
+        $("#subjectsInput").sendKeys(" 1254");
+        //$(".subjects-auto-complete__input").setValue("Swimming");
+        //$(".subjects-auto-complete__input").sendKeys(" Swimming");
+
+
+
+
+//подправить конечные проверки
+       // $("#output").shouldHave(text("Vova Bobin"), text("Vova@mail.ru"),
+         //       text("Omsk 19"), text("Another adress"));
 }}
