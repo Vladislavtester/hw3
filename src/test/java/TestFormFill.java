760dc7bdd9761811c4ed4317e6f6b44e3e23f90d
Vladislav Nazarov <nazzzap@gmail.com>
2023-03-30 20:16:58 +0600

    Final version

--- src/test/java/TestFormFill.java
+++ src/test/java/TestFormFill.java
@@ -1,45 +1,54 @@
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.commands.Find;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestFormFill {
     @BeforeAll
 static void beforeAll(){
 
-    Configuration.browserSize = "1920x1080";
-    Configuration.baseUrl = "https://demoqa.com" ;   }
+    Configuration.browserSize = "1080x1080";
+    Configuration.baseUrl = "https://demoqa.com" ;
+    Configuration.holdBrowserOpen=true ;
+    }
 
     @Test
-    void fillFormTest() throws InterruptedException {
+    void fillFormTest()  {
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
 
         $("#firstName").setValue("Vova");
         $("#lastName").setValue("Kashin");
         $("#userEmail").setValue("Kashin@mail.ru");
-        $("#gender-radio-1").sendKeys(" ");
-        $("#userNumber").val("12345");
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").val("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
         $(".react-datepicker__month-select").selectOption("March");
-        $(".react-datepicker__day--015").click();
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").click();
         $(".react-datepicker__year-select").selectOption("2007");
-        $(".subjects-auto-complete__value-container").click();
-        $("#subjectsInput").sendKeys(" 1254");
-        //$(".subjects-auto-complete__input").setValue("Swimming");
-        //$(".subjects-auto-complete__input").sendKeys(" Swimming");
+        $(".react-datepicker__day--015:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("bio").pressEnter();
+        $("#hobbiesWrapper").$(byText ("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("photo.jpg");
+        $("#currentAddress").setValue("Pushkina 12");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText ("Haryana")).click();
+        $("#city").click();
+        $("#city").$(byText ("Panipat")).click();
+        $("#submit").click();
 
+        //Проверки
+        $(".modal-body").should(appear);
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(text("Kashin@mail.ru"));
+        $(".modal-body").shouldHave(text("1234567890"));
+        $(".modal-body").shouldHave(text("Pushkina 12"));
 
-
-
-//подправить конечные проверки
-       // $("#output").shouldHave(text("Vova Bobin"), text("Vova@mail.ru"),
-         //       text("Omsk 19"), text("Another adress"));
 }}
